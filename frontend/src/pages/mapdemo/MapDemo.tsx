457603d64b71844e96b0dea5d6d19a77813d8c8d
Christian Weck <weck.chr@gmail.com>
2023-05-08 15:58:03 +0200

    replaced initial center point with useStat LatLngTuple

--- frontend/src/pages/mapdemo/MapDemo.tsx
+++ frontend/src/pages/mapdemo/MapDemo.tsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
+import {LatLng, LatLngTuple} from "leaflet";
 
 export default function MapDemo() {
 
+    const [mapCenterPosition, setMapCenterPosition] = useState<LatLngTuple>([51.505, -0.09]);
+
     return (
         <div>
-            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{width: 500, height: 500}}>
+            <MapContainer center={mapCenterPosition} zoom={13} scrollWheelZoom={false} style={{width: 500, height: 500}}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={[51.505, -0.09]}>
+                <Marker position={mapCenterPosition}>
                     <Popup>
                         A pretty CSS3 popup. <br /> Easily customizable.
                     </Popup>
